d17d150110c280a14f7c3ee0af0aea51cb9b4cfe
Steven Huang <stevenhuang4321@gmail.com>
2014-11-06 23:41:43 +0800

    add read_raw.m( needed test)
    read data from Raw format file.

---
+++ aux_func/read_raw.m
@@ -0,0 +1,15 @@
+function [ raw_data ] = read_raw( file_dir, raw_info )
+%READ_RAW read data from Raw format file.
+%   Usuage: 
+%   [ raw_data ] = read_raw( file_dir, raw_info )
+% where:
+%   file_dir = 'C:\1.dat'
+%   raw_info.img_width = 3800;
+%   raw_info.img_height = 3800;
+%   raw_info.img_type ='int16';
+fileID = fopen(file_dir);
+raw_data = fread(fileID,[raw_info.img_width,raw_info.img_height],raw_info.img_type);% full image
+fclose(fileID);
+
+end
+
