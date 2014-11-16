3a1115e5d2cb9dee1f8e2d6657a4fa2c31a46f0f
Steven Huang <stevenhuang4321@gmail.com>
2014-11-16 21:32:54 +0800

    calculate raw image information

--- src/cal_raw.m
+++ src/cal_raw.m
@@ -1,15 +1,16 @@
-function [Y0_s,Y1_s ] = cal_raw( dir_raw,raw_info,preproc_info,I_background,ImgFiles,numsteps,No )
+function [Y0_s,Y1_s ] = cal_raw( dir_raw,raw_info,preproc_info,I_background,ImgFiles,num_steps,No )
 %CAL_RAW 
 % 
-
+j = 1;
 for i= No: (No+num_steps-1)
-    I_0 =read_specific_file( ImgFiles{i}.name, raw_info, 1  );
+%     I_0 =read_specific_file( ImgFiles{i}.name, raw_info, 1  );
+    I_0 = read_raw(ImgFiles(i).name,raw_info);
     I_1 = I_0 - I_background; % Background correction.
     
    I_2 = imrotate(I_1,preproc_info.rotate_angle,'bilinear','crop'); % Rotate Image.
     
-   I_s(:,:,i) = I_2(preproc_info.ROI_xmin:preproc_info.ROI_xmax,...
-        preproc_info.ROI_ymin:preproc_info.ROI_ymax) ;
+   I_s(:,:,j) = imcrop(I_2,preproc_info.ROI);
+   j = j+1;
 end
 Y = fft(I_s,[],3);
 Y0_s = Y(:,:,1);
