63c0a7da43f0401d3be56690f2758e3c1ea4aeb8
Steven Huang <stevenhuang4321@gmail.com>
2014-11-18 00:04:04 +0800

    commit on recon absorption

--- src/recon3D_absorp.m
+++ src/recon3D_absorp.m
@@ -1,9 +1,9 @@
-function [ output_args ] = recon3D_absorp(  dir_darkfield,handles )
+function [ proj_cali, R] = recon3D_absorp(  dir_absorp,handles,edge_left,edge_right )
 
-AbsoPath = strcat(dir_darkfield,'\*.mat');
+AbsoPath = strcat(dir_absorp,'\*.mat');
 ImgFiles = dir(AbsoPath); % a address list of each file.
 numfiles = length(ImgFiles); %% number of files
-cd (dir_darkfield);
+cd (dir_absorp);
 
 load(ImgFiles(1).name);
 [imWidth, imHeight] = size(data);
@@ -11,9 +11,18 @@ I = single(zeros(imWidth,imHeight,numfiles));
 for i = 1:numfiles
     load(ImgFiles(i).name);
     I(:,:,i) = single(data);
+    script = sprintf('%d',i);
+    disp(i);
 end
-refer_section = floor(600);
-[ proj_cali ] = axis_cali( I,refer_section );
+% refer_section = floor(600);
+% [ proj_cali ] = axis_cali( I,refer_section );
+allownance = 200;
+PMMAWidth_mean = 507;
+for i = 1:numfiles
+    img_LeftIndex(i) = edge_left(i) - floor(allownance/2);%Lefe_edge
+    img_RightIndex(i) = img_LeftIndex(i) + PMMAWidth_mean+allownance;%right_edge
+    proj_cali(:,:,i) =  I(:,img_LeftIndex(i):img_RightIndex(i),i);
+end
 [~,imHeight_2,~] = size(proj_cali);
 for i = 1:imWidth
     I_section = reshape(proj_cali(i,:,:),imHeight_2,numfiles);
@@ -21,8 +30,8 @@ for i = 1:imWidth
     disp(script);
     R(:,:,i) = iradon(I_section,0:0.3581:179,'Ram-Lak');
 end
-save('heloworld.mat','R');
-end
 
 end
+
+
 
