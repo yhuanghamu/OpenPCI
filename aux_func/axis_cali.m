15836df0b853ef244da68e97c73110216198fd57
Steven Huang <stevenhuang4321@gmail.com>
2014-11-17 13:43:59 +0800

    modify axis calibration function

--- aux_func/axis_cali.m
+++ aux_func/axis_cali.m
@@ -11,15 +11,15 @@ refer_2 = refer(floor(end/2)+1:end);
 edge_1 = find(refer_1 == min(refer_1));
 edge_2 = find(refer_2 == min(refer_2))+floor(imHeight/2);
 PMMAWidth = edge_2(1) - edge_1(1);
-allownance= (imHeight-PMMAWidth)/2;
+allownance= floor((imHeight-PMMAWidth)/2);
 
 
-proj_cali = uint8(zeros(imwidth,PMMAWidth+allownance+1,numproj));
+proj_cali = zeros(imwidth,PMMAWidth+allownance+1,numproj);
 for i = 1:numproj
     img = refer_section(:,i);
     img_1 = img(1:floor(end/2));
     img_LeftIndex = find(img_1 == min(img_1));
-    img_LeftIndex = img_LeftIndex(1) - allownance/2;%Lefe_edge
+    img_LeftIndex = img_LeftIndex(1) - floor(allownance/2);%Lefe_edge
     img_RightIndex = img_LeftIndex + PMMAWidth+allownance;%right_edge
     %proj2d_3(:,i) = proj2d_0(img_LeftIndex(i):img_RightIndex(i),i);
     proj_cali(:,:,i) = projection(:,img_LeftIndex:img_RightIndex,i);
